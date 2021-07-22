c096039b5550973f7790c2d73baa790393818ae2
bdrkreza <diureza@gamil.com>
2021-07-22 17:12:37 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
+import { ITask } from "./components/Interface/interface";
+import TodoTask from "./components/todoList/TodoTask";
 
-function App() {
+
+const App:FC = () => {
+    const [task, setTask] = useState<string>("");
+    const [deadline, setDeadline] = useState<number>(0);
+    const [todoList, setTodoList] = useState<ITask[]>([]);
+  
+  const handleChange = (event:ChangeEvent<HTMLInputElement>):void =>{
+  if(event.target.name==="task"){
+    setTask(event.target.value)
+  }else{
+    setDeadline(Number(event.target.value))
+  }
+  } ;
+  
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList( [ ...todoList,  newTask] );
+    setTask("");
+    setDeadline(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="dashboard">
+	<div className="dashboard-left">
+		<div className="left-header">
+			<h3>Create a task!</h3>
+		</div>
+		<div className="left-task">
+			<div className="task-info task-item">
+				<label  className="task-label">Task</label>
+				<input onChange={handleChange}
+        name="task" type="text"
+         className="task-info-input task-input" 
+        placeholder='Go to the gym...' required
+        
+        />
+				<label  className="task-label">Tag</label>
+				<input onChange={handleChange} 
+        name="deadline"  type="number"
+         className="task-input task-type-input"
+          placeholder="Deadline.." required/>
+			</div>
+			<button onClick={addTask} className="task-add">Add Task</button>
+		</div>
+	</div>
+	<div className="dashboard-right">
+  {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+
+	</div>
+</div>
   );
-}
+};
 
 export default App;
